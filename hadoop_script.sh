f1e822892b84b1b5dd60232f20045f7d93f17fee
Maki <marko.mihajlovic@elfak.rs>
2018-05-31 09:45:56 +0200

    todo added

---
+++ hadoop_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# create directory on hdfs
+# hadoop fs -mkdir /KMeans/ .... if not excist
+
+# copy local input files
+
+# move local jar file
+# hadoop fs -put ./Resources/Input/*.txt /KMeans/Resources/Input
+
+# execute jar file
+
+# print results
